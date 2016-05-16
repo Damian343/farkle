b30fb158730f1ae02160994ba7093c8e154d75c1
Damian343 <damianhamilton2@gmail.com>
2016-05-15 23:05:51 -0700

    Handled a particular error.

--- src/FarkleGame/gameBoard.java
+++ src/FarkleGame/gameBoard.java
@@ -13,13 +13,16 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-
+//each player class keeps track of score, random player is selected to go first
 public class gameBoard extends JFrame {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private JPanel contentPane;
 	private JTextField textField;
-	private int dice[];
-	private static JLabel dice1, dice2, dice3, dice4, dice5, dice6;
+	private int[] diceSides;
+	private int[] dice;
+	private boolean buyIn;
+	private Random random;
+	private static JLabel dice1, dice2, dice3, dice4, dice5, dice6, currentPlayerlbl;
 	private int numDice;
 	
 	//called from main menu, creates the players, starts game
@@ -27,8 +30,8 @@ public class gameBoard extends JFrame {
 		setTitle("Damians farkle");
 		players.add(new Player(nameOne));
 		players.add(new Player(nameTwo));
-		dice = new int[6];
-		numDice = 6;
+		this.buyIn = buyIn;
+		random = new Random(); 
 		createWindow();
 	}
 	//creates the gameboard where all info is displayed
@@ -46,95 +49,107 @@ public class gameBoard extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblPlayerScore = new JLabel("Player 1 Score");
-		lblPlayerScore.setBounds(292, 122, 76, 14);
-		contentPane.add(lblPlayerScore);
+		JLabel playerScorelbl = new JLabel("Players Score");
+		playerScorelbl.setBounds(292, 122, 76, 14);
+		contentPane.add(playerScorelbl);
 		
-		JLabel lblPlayerScore_1 = new JLabel("Player 2 score");
-		lblPlayerScore_1.setBounds(292, 160, 71, 14);
-		contentPane.add(lblPlayerScore_1);
-		
-		JLabel lblAvailableCombinations = new JLabel("Available Combinations");
+		JLabel lblAvailableCombinations = new JLabel("Available Combinations: ");
 		lblAvailableCombinations.setBounds(24, 122, 119, 14);
 		contentPane.add(lblAvailableCombinations);
 		
-		JLabel lblCombinations = new JLabel("Combinations");
-		lblCombinations.setBounds(158, 122, 76, 14);
-		contentPane.add(lblCombinations);
+		JLabel combinationlbl = new JLabel("");
+		combinationlbl.setBounds(158, 122, 76, 14);
+		contentPane.add(combinationlbl);
 		
-		JLabel lblAvailablePointsIn = new JLabel("available points in roll");
+		JLabel lblAvailablePointsIn = new JLabel("Total Roll Score: ");
 		lblAvailablePointsIn.setBounds(24, 160, 119, 14);
 		contentPane.add(lblAvailablePointsIn);
 		
-		JLabel lblRollpoints = new JLabel("rollPoints");
-		lblRollpoints.setBounds(158, 160, 57, 14);
-		contentPane.add(lblRollpoints);
+		JLabel rollPointslbl = new JLabel("");
+		rollPointslbl.setBounds(158, 160, 57, 14);
+		contentPane.add(rollPointslbl);
 		
-		JLabel lblPleseSelectCombinations = new JLabel("Plese select combinations");
+		JLabel lblPleseSelectCombinations = new JLabel("Plese select dice");
 		lblPleseSelectCombinations.setBounds(10, 197, 138, 14);
 		contentPane.add(lblPleseSelectCombinations);
-		
-		textField = new JTextField();
-		textField.setBounds(139, 194, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
 		
 		JLabel lblFarkleCount = new JLabel("Farkle Count");
 		lblFarkleCount.setBounds(292, 197, 71, 14);
 		contentPane.add(lblFarkleCount);
 		
-		JLabel lblCount = new JLabel("Count");
-		lblCount.setBounds(360, 197, 46, 14);
-		contentPane.add(lblCount);
+		JLabel farkleCountlbl = new JLabel("");
+		farkleCountlbl.setBounds(360, 197, 46, 14);
+		contentPane.add(farkleCountlbl);
 		
 		JButton btnRoll = new JButton("Roll");
 		btnRoll.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//diceRoll(dice1);diceRoll(dice2);
+				diceRoll(dice1);diceRoll(dice2);diceRoll(dice3);diceRoll(dice4);diceRoll(dice5);diceRoll(dice6);
 			}
 		});
 		btnRoll.setBounds(84, 227, 89, 23);
 		contentPane.add(btnRoll);
 		
 		JButton btnPass = new JButton("Pass");
+		btnPass.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				passTurn();
+			}
+		});
 		btnPass.setBounds(260, 227, 89, 23);
 		contentPane.add(btnPass);
 		
-		JLabel dice1 = new JLabel("yolo");
+		dice1 = new JLabel("");
 		dice1.setBounds(24, 46, 46, 42);
 		contentPane.add(dice1);
 		
-		JLabel dice2 = new JLabel("yolo");
+		dice2 = new JLabel("");
 		dice2.setBounds(97, 46, 46, 42);
 		contentPane.add(dice2);
 		
-		JLabel dice3 = new JLabel("yolo");
+		dice3 = new JLabel("");
 		dice3.setBounds(169, 46, 46, 42);
 		contentPane.add(dice3);
 		
-		JLabel dice4 = new JLabel("yolo");
+		dice4 = new JLabel("");
 		dice4.setBounds(237, 46, 46, 42);
 		contentPane.add(dice4);
 		
-		JLabel dice5 = new JLabel("yolo");
+		dice5 = new JLabel("");
 		dice5.setBounds(303, 46, 46, 42);
 		contentPane.add(dice5);
 		
-		JLabel dice6 = new JLabel("yolo");
+		dice6 = new JLabel("");
 		dice6.setBounds(372, 46, 46, 42);
 		contentPane.add(dice6);
-		diceRoll(dice1);
+		
+		currentPlayerlbl = new JLabel("");
+		currentPlayerlbl.setBounds(158, 21, 125, 14);
+		contentPane.add(currentPlayerlbl);
 		//messy need to clean
 		setVisible(true);
+		if(buyIn) { buyInNewGame(); } else { newGame(); }
+	}
+	//Player passes Setpoints along with numdice to next player
+	public void passTurn() {
+		
+	}
+	//need 500 points in order to start adding to score
+	public void buyInNewGame() {
+		
+	}
+	//creates new game sets scores number dice also labels no 500 point buy in
+	public void newGame() {
+		dice = new int[6];
+		diceSides = new int[6];
+		numDice = 6;
+		currentPlayerlbl.setText("currently " + players.get(random.nextInt(1)).getName() + "'s turn");
 	}
 	//rolls dice adds that dice image to label
 	public void diceRoll(JLabel dieLabel) {
-		Random rnd = new Random();
-		int n = rnd.nextInt(6) + 1;
-
+		int n = random.nextInt(6) + 1;
 		//gets the wanted image stores it as icon then places in label
 		String neededImage = "C:\\Users\\damian\\Desktop\\lab11\\dice" + n + ".png";
-
 		dieLabel.setIcon(new ImageIcon(neededImage));
 		dice[n-1]++;
 	}
--- src/FarkleGame/gameStart.java
+++ src/FarkleGame/gameStart.java
@@ -17,22 +17,21 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.Color;
 import javax.swing.UIManager;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 
-public class gameStart extends JFrame {
+public class gameStart extends JFrame{
 	private JPanel contentPane;
 	private JTextField playerOneName;
 	private JTextField playerTwoName;
 
 	public gameStart() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 207, 250);
+		setBounds(100, 100, 193, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -40,7 +39,7 @@ public class gameStart extends JFrame {
 		
 		JLabel lblWelcomeToFarkle = new JLabel("Welcome to Farkle!");
 		lblWelcomeToFarkle.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblWelcomeToFarkle.setBounds(36, 21, 122, 14);
+		lblWelcomeToFarkle.setBounds(36, 21, 108, 14);
 		contentPane.add(lblWelcomeToFarkle);
 		
 		JRadioButton buyInBttn = new JRadioButton("Buyin(500)");
@@ -48,20 +47,32 @@ public class gameStart extends JFrame {
 		buyInBttn.setBounds(48, 140, 92, 23);
 		contentPane.add(buyInBttn);
 
-		playerOneName = new JTextField();
-		playerOneName.setText("Player 1 Name");
-		playerOneName.setBounds(48, 57, 92, 20);
+		playerOneName = new JTextField("  Player Ones Name");
+		playerOneName.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				playerOneName.setText("");
+			}
+			public void focusLost(FocusEvent e) {
+			}
+		});
+		playerOneName.setBounds(36, 57, 108, 20);
 		contentPane.add(playerOneName);
 		playerOneName.setColumns(10);
 		
-		playerTwoName = new JTextField();
-		playerTwoName.setText("Player 2 Name");
-		playerTwoName.setBounds(48, 104, 92, 20);
+		playerTwoName = new JTextField("  Player Twos Name");
+		playerTwoName.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				playerTwoName.setText("");
+			}
+			public void focusLost(FocusEvent e) {
+			}
+		});
+		playerTwoName.setBounds(36, 104, 109, 20);
 		contentPane.add(playerTwoName);
 		playerTwoName.setColumns(10);
 		
 		JButton btnStartGame = new JButton("Start Game");
-		btnStartGame.setBounds(48, 177, 92, 23);
+		btnStartGame.setBounds(36, 177, 109, 23);
 		contentPane.add(btnStartGame);
 		btnStartGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
